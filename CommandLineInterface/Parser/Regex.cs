52c85883003f50d0b8b493d1f4422a9d126fcbdf
Oliver Iliffe <oli_iliffe@hotmail.com>
2021-01-29 17:36:54 +0000

    Added regular expressions for parsing

--- CommandLineInterface/Parser/Regex.cs
+++ CommandLineInterface/Parser/Regex.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
 
 namespace CommandLineInterface
 {
     public static partial class Parser
     {
-        public static class Regex
+        public static class RegularExpressions
         {
-            
+            private static string _string_      = "\".*?[^\\\\]\"";
+            private static string _variable_    = @"[\w_\.\/][\w_\.\-\/]*";
+            private static string _tag_         = @"\-[\w_]+";
+            private static string _commandName_ = @"@?[\w_]+";
+            private static string _argument_    = $"({_string_}|{_variable_})";
+            private static string _arguments_   = $"({_argument_}( +|$))";
+
+            // Separates the command name from its arguments (tags, default arguments etc.)
+            // USEFUL DATA:
+            //  [1] The command name
+            //  [2] The remaining part of the string
+            public static Regex SeparateCommandNameFromArguments
+                = new Regex($"^({_commandName_})(.*)$");
+
+            // 'scope' referring to the tag that they are bound to. 
+            // Alternatively, an argument can be bound to the default scope, meaning it has no preceeding tag.
+            // USEFUL DATA:
+            //  Match[] of all scopes
+            public static Regex GetScopes
+                = new Regex($"(^{_arguments_}+|{_tag_}( +{_arguments_})*)");
+
+            // Separates the tag name from its positional arguments
+            // USEFUL DATA:
+            //  [1] The tag name
+            //  [2] The tag's arguments
+            public static Regex SeparateTagFromArguments
+                = new Regex($"^({_tag_})( +{_arguments_}+)?");
+
+            // Matches all arguments
+            // USEFUL DATA:
+            //  [1] Trimmed argument
+            public static Regex GetArguments
+                = new Regex($"(^| )({_argument_})( |$)");
+
+            public static void PrintCompiledRegularExpressions()
+            {
+                Console.WriteLine(
+                    "SeparateCommandNameFromArguments | " + $"^({_commandName_})(.*)$"                     + "\n" +
+                    "GetScopes                        | " + $"(^{_arguments_}+|{_tag_}( +{_arguments_})*)" + "\n" +
+                    "SeparateTagFromArguments         | " + $"^({_tag_})( +{_arguments_}+)?"               + "\n" +
+                    "GetArguments                     | " + $"(^| )({_argument_})( |$)"                    + "\n"
+                    );
+            }
+
+            // Returns ranges (`List<(int, int)>` types) to represent all matches that are not in any of the specified ranges
+            public static string MatchIfNotInRanges(Regex rx, string text, List<(int, int)> ranges)
+            {
+                return null;
+            }
         }
     }
 }
--- CommandLineInterface/Parser/StringTools.cs
+++ CommandLineInterface/Parser/StringTools.cs
@@ -16,7 +16,7 @@ namespace CommandLineInterface
         {
             public static List<(int, int)> StringRanges(string text, char[] quotes = null, char escapeChar = '\\')
             {
-                quotes = quotes ?? new char[] { '\'', '"' };
+                quotes = quotes ?? new char[] { '\'', '"', '`' };
                 int charIndex; 
                 int maxIndex = text.Length;
                 char thisChar;
@@ -86,7 +86,7 @@ namespace CommandLineInterface
                 ranges.Reverse();
                 foreach (var range in ranges)
                 {
-                    text = ReplaceRange(text, callback(Slice(text, range)), range);
+                    text = ReplaceRange(text, callback( Slice(text, (range.Item1, range.Item2 + 1)) ), range);
                 }
                 return text;
             }
@@ -127,6 +127,101 @@ namespace CommandLineInterface
                 }
 
                 return gapRanges;
+            }
+        
+            private static string Jumbled(int length, char[] chars)
+            {
+                StringBuilder sb = new StringBuilder("", length);
+                Random random = new Random();
+                for (int i = 0; i < length; i++)
+                {
+                    sb.Append(chars[random.Next(chars.Length)]);
+                }
+                return sb.ToString();
+            }
+
+            private static Func<string, string> UnescaperFactory(string[] escapeCodes, Dictionary<string, char> unescapeDictionary)
+            {
+                return escapedText =>
+                {
+                    if (escapedText.Length < 8) { return escapedText; };
+                    int i;
+                    for (i = 0; i < escapeCodes.Length; ++i)
+                    {
+                        string thisCode = escapeCodes[i];
+                        //Console.WriteLine($"escaping code \"{thisCode}\"");
+                        escapedText = escapedText.Replace(thisCode, unescapeDictionary[thisCode].ToString());
+                    }
+                    return escapedText;
+                };
+            }
+
+            static private char[] charsToJumble = new char[]
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_'
+            };
+
+            public static Func<string, string> EscaperFactory(char[] toEscape, out Func<string, string> unEscape, char escapeChar = '\\')
+            {
+                int i;
+
+                int toEscape_Length = toEscape.Length;
+                string[] generatedEscapeCodes = new string[toEscape_Length];
+                int escapeCodeCount = 0;
+                string escapeCode;
+                while (escapeCodeCount < toEscape_Length)
+                {
+                    escapeCode = Jumbled(8, charsToJumble);
+                    if (!generatedEscapeCodes.Contains(escapeCode))
+                    {
+                        generatedEscapeCodes[escapeCodeCount] = escapeCode;
+                        ++escapeCodeCount;
+                    }
+                }
+
+                Dictionary<char, string> lookupDictionary = new Dictionary<char, string>();
+                Dictionary<string, char> reverseLookupDictionary = new Dictionary<string, char>();
+                for (i = 0; i < toEscape_Length; ++i)
+                {
+                    lookupDictionary[toEscape[i]] = generatedEscapeCodes[i];
+                    reverseLookupDictionary[generatedEscapeCodes[i]] = toEscape[i];
+                }
+
+                unEscape = UnescaperFactory(generatedEscapeCodes, reverseLookupDictionary);
+
+                return textToEscape => 
+                {
+                    int i2;
+                    bool escapeThis = false;
+                    StringBuilder sb = new StringBuilder();
+                    for (i2 = 0; i2 < textToEscape.Length; ++i2)
+                    {
+                        if (textToEscape[i2] == escapeChar)
+                        {
+                            escapeThis = true;
+                            continue;
+                        }
+                        if (escapeThis)
+                        {
+                            sb.Append(lookupDictionary[textToEscape[i2]]);
+                            escapeThis = false;
+                            continue;
+                        }
+                        sb.Append(textToEscape[i2]);
+                    }
+
+                    return sb.ToString();
+                };
+            }
+        
+            public static string EscapeIfInString(string text, Func<string, string> Escaper)
+            {
+                List<(int, int)> stringRanges = StringRanges(text);
+                return ReplaceRanges(text, stringRanges, portion => Escaper(portion));
             }
         }
     }
